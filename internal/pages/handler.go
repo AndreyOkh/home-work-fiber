cf41b969cdbf695fe1e29a40776ac1fed35316e8
Andrey <andrey1q2q@gmail.com>
2025-02-23 19:29:31 +1000

    2-Logger

--- internal/pages/handler.go
+++ internal/pages/handler.go
@@ -2,19 +2,23 @@ package pages
 
 import (
 	"github.com/gofiber/fiber/v2"
+	"log/slog"
 )
 
 type Handler struct {
 	router fiber.Router
+	log    *slog.Logger
 }
 
-func NewHandler(router fiber.Router) {
+func NewHandler(router fiber.Router, log *slog.Logger) {
 	h := &Handler{
 		router: router,
+		log:    log,
 	}
 	h.router.Get("/", h.mainPage)
 }
 
 func (h *Handler) mainPage(c *fiber.Ctx) error {
+	h.log.Warn("Test")
 	return c.SendString("Home Page")
 }
